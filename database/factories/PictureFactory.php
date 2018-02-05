71793c38ff152b424824fd4927f5e975a3d2b495
Unknown <christo.navas@hotmail.fr>
2018-02-05 22:24:37 +0100

    Update
    Correction de bug

--- database/factories/PictureFactory.php
+++ database/factories/PictureFactory.php
@@ -6,7 +6,9 @@ $factory->define(App\Picture::class, function (Faker $faker) use ($factory) {
     //http://lorempixel.com/
     $random = str_random(12);
     $link = $random . '.jpg';
+
     $file = file_get_contents($faker->imageUrl(800, 800));
+
     Storage::disk("local")->put($link, $file);
 
     return [
